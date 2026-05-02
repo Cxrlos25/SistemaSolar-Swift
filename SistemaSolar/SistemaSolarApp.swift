eb9c564d964831d8b102cb26d95ff8de1926c95f
carlos <ndrehinos25@hotmail.com>
2026-05-02 12:14:00 -0500

    Initial Commit

---
+++ SistemaSolar/SistemaSolarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SistemaSolarApp.swift
+//  SistemaSolar
+//
+//  Created by Carloss on 2/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SistemaSolarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SistemaSolarTests/SistemaSolarTests.swift
@@ -0,0 +1,16 @@
+//
+//  SistemaSolarTests.swift
+//  SistemaSolarTests
+//
+//  Created by Carloss on 2/05/26.
+//
+
+import Testing
+
+struct SistemaSolarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
